42dc19125832e78fc04e709c30eae6a94c917bef
Joanna Cortes <jococopo@gmail.com>
2023-03-28 14:53:44 -0600

    General setup and TDD Employee obj

---
+++ lib/employee.rb
@@ -0,0 +1,18 @@
+require 'pry'
+class Employee
+  attr_reader :type,
+              :weekly_availability
+
+  def initialize(type, weekly_availability)
+    @type = type
+    @weekly_availability = weekly_availability
+  end
+
+  def available?(weekly_availability, day)
+    status = weekly_availability.find {|k, v| k == day }
+    # status = nil
+    # weekly_availability.each {|key, value| status = value if day == key}
+    status[1] == :non_pto ? true : false
+  end
+end
+{foo: 0, bar: 1, baz: 2}.find {|key, value| key.start_with?('b') }    
---
+++ spec/schedule_spec.rb
@@ -0,0 +1,23 @@
+require "./lib/schedule"
+
+RSpec.describe Schedule do
+  it 'schedules a single story house' do
+    # results = Schedule.new([building_array], [employee_array])
+    # expect(results).to eq({schedule_object})
+  end
+
+
+  it 'create a schedule for the next 5 days' do
+  end
+
+  it 'specifies employee availablity' do
+  end
+
+  it 'specifies employee type' do
+  end
+
+  # buidling requirements to be handled by Building class
+
+  
+
+end
