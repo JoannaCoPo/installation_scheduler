b583963e10e5d3f5cb6129788d780192e1273000
Joanna Cortes <jococopo@gmail.com>
2023-03-28 22:42:15 -0600

    Remove pseudocode and consolidate for time

--- lib/setup_by_day.rb
+++ lib/setup_by_day.rb
@@ -1,37 +1,45 @@
 class SetupByDay
-  attr_reader :day, 
-              :buidling,
-              :employee_pool, 
-              :assigned_employees
+  attr_reader :results
               
-  def initialize(day, buidling, employee_pool)
-    @day = day
-    @buidling = buidling
-    @employee_pool = employee_pool
-    @assigned_employees = []
+  def initialize
+    @results = { }
   end
 
-  def filter_by_availability
-    employee_pool.filter_map {|emp| emp if emp.available?(day) }
-  end
+  def schedule(buildings, employees)
+    buildings.each do |building|
+      # get available employees for each new building 
+      available_employees = filter_by_availability(employees, building)
+      check_building_requirements(building, available_employees)
 
-  def check_buidling_requirements
-    if building.type == single_story
-      check_single_story_compatibility
-    elsif building.type == two_story
-      check_two_story_compatibility
-    else
-      check_commericial_compatibility
+      building.requirements.each do |position|
+        # if employee is available on @scheduling_for_day and has the position, assign the employee to the building
+          #available_employees.each and check if the employee is available?
+          available_employees.each do |emp|
+            if emp.type == position
+              building.assigned_employees << emp
+              available_employees.delete(emp)
+            end
+          end
+          # if available, assign to the building, and I guess the building also needs to know what day of the week it's getting worked on. So building might have another attribute like 'day' that could be set to 'monday'
+        # if the employee is not a match, check the next one. If no employees are available, I guess you could either move on to the next building, or on to the next day
+      end
     end
   end
 
-  # def check_single_story_compatibility
-  #   buidling.requirements 
-  # end
+  def filter_by_availability(employees, building)
+    employees.filter_map {|emp| emp if emp.available?(building.day) }
+  end
 
-  def check_two_story_compatibility
-    binding.pry
-    filter_by_availability
-    buidling.requirements 
+  def check_building_requirements(building, available_employees)
+    if (building.requirements.class == Hash) && (building.requirements.has_key?(:required))
+      building.requirements[:required].each do |req|
+        available_employees.any? do|emp| 
+          if emp.type == req
+            building.assigned_employees << emp
+            available_employees.delete(emp)
+          end 
+        end
+      end
+    end
   end
 end
