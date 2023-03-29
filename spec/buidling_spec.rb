fbbd821c657ef64e16b93ce29ff87b326ec13377
Joanna Cortes <jococopo@gmail.com>
2023-03-29 08:44:24 -0600

    Cleanup Building tests

--- spec/buidling_spec.rb
+++ spec/buidling_spec.rb
@@ -1,30 +1,30 @@
 require "./lib/building"
 
 RSpec.describe Building do
-#   before(:each) do 
-#     @building = Building.new(:single_story)
-#   end
   describe '#initialize' do
-    single_story = Building.new(:single_story)
+    single_story = Building.new(:single_story, 'monday')
 
     it 'returns building type' do
       expect(single_story.type).to eq(:single_story)
     end
 
     it 'returns requirements per buidling type' do
-      expect(single_story.requirements).to eq(["certified_installer"])
-      two_story = Building.new(:two_story_home)
-      expect(two_story.requirements).to eq(["certified_installer"])
+      expect(single_story.requirements).to eq([:certified])
+      two_story = Building.new(:two_story_home, 'tuesday')
+    #   binding.pry
+      expect(two_story.requirements).to eq({
+        requirements:{
+          1=>[:certified, :pending_certification], 
+          2=>[:certified, :pending_certification]
+        },
+        four_of_any: [:certified, :pending_certification, :laborer, :certified]
+        })
     end
   end
 
   describe '#available?' do
     xit 'returns employee availability per day' do
-      # day = Day.new('Tuesday') 
-      tuesday = :tuesday
-      expect(@employee.available?(@weekly_availability, tuesday)).to eq(true)
+      # would need to backfill this test
     end
   end
-
-  private
 end
